ff80660b5493f9690a064d40587830407f75eb5c
masakiharuto <masakiharuto@bmcomp.jp>
2025-04-19 17:05:11 +0900

    Add files via upload

---
+++ c_test/c3_2/3-2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main(int argc, char *argv[]){
+    int n = atoi(argv[1]);
+    int* list;
+    list = (int*)malloc(sizeof(int) * n);
+    for(int i =1; i < n; i++){
+        list[i] = 0;
+    }
+    int list_len = 1;
+    int num = 3;
+    list[0] = 2;
+    for(int i = 1; i <= n - 1; i++){
+        bool cut = true;
+        for(int k = 0; list_len >= k; k++){
+            if( num % list[k] == 0){
+                cut = false;
+            };
+        };
+        if( cut == false ){
+            i--;
+        }else{
+            list[i] = num;
+            list_len++;
+        };
+        num += 2;
+    };
+    printf("%d\n",list[ n - 1]);
+    return 0;
+    free(list);
+}
